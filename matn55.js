27037b44b2f804cda02a24607a7cf7ba5b74f7fc
AlirezaZarrin <alirezazarrin1144@gmail.com>
2022-01-22 10:05:57 -0800

    first commit

---
+++ matn55.js
@@ -0,0 +1,15 @@
+const matn = 'dars emroz rajeb function hastesh nazar shoma rajeb function chiye'
+let aval = 'emroz rajeb'
+let akhar = 'hastesh'
+
+
+
+function avalakar() {
+    
+    console.log(matn.slice(matn.indexOf(aval),matn.indexOf(akhar)+(akhar.length)))
+}
+
+avalakar()
+
+
+
